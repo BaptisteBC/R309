5e79da286943dec1b1b6045d5405f65ec08748b7
BaptisteBC <baptiste.blanccarrier@gmail.com>
2023-11-07 11:14:17 +0100

    1st

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-import sys
-
-
 def divEntier(x: int, y: int) -> int:
-    if y == 0:
-        raise ZeroDivisionError
+    if x < y:
+        return 0
     else:
-
-        if x < y:
-            return 0
-        else:
-            x = x - y
-            return divEntier(x, y) + 1
+        x = x - y
+        return divEntier(x, y) + 1
 
 
 if __name__ == '__main__':
-    try:
-        a = int(input("a: "))
-        b = int(input("b: "))
-        div = divEntier(a, b)
-    except ValueError:
-        print("Valeur de l'argument incorrect")
-        while ValueError:
-            try:
-                a = int(input("a: "))
-                b = int(input("b: "))
-                div = divEntier(a, b)
-            except ValueError:
-                pass
-            else:
-                print(div)
+    flag = False
+    while not flag:
+        try:
+            a = int(input("Saisir a: "))
+        except ValueError:
+            print("a n'est pas un entier")
+        else:
+            flag = True
+    flag = False
+    while not flag:
+        try:
+            b = int(input("Saisir b: "))
+            div = divEntier(a, b)
+        except ValueError:
+            print("b n'est pas un entier")
+        except RecursionError:
+            print("b doit être différent de 0")
+        else:
+            flag = True
 
-    else:
-        print(div)
+    print(f"Résultat : {div}")
