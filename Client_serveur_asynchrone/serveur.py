c2a131828826864ab7508361929cea5ea877223d
baptisteBC <baptiste.blanc-carrier@uha.fr>
2023-11-17 15:32:02 +0100

    serveur asynch thread receive (fct)

--- Client_serveur_asynchrone/serveur.py
+++ Client_serveur_asynchrone/serveur.py
@@ -1,38 +1,30 @@
 import socket
+import threading
+
+def receive(conn):
+    msg = ""
+    while msg != "bye" and msg != "stop":
+        msg = conn.recv(1024).decode()
+        host = socket.gethostname()
+        print(f"Client {host} : {msg}")
 
 if __name__ == '__main__':
     flag = False
     port = 10000
+    msg = ""
+    reply = ""
 
     server_socket = socket.socket()
     print("Serveur ouvert")
     server_socket.bind(('0.0.0.0', port))
+
     server_socket.listen(1)
     conn, address = server_socket.accept()
 
-    while not flag:
+    while msg != "bye" and msg != "stop" and reply != "bye" and reply != "stop":
+        t1 = threading.Thread(target=receive, args=[conn])
+        t1.start()
 
-        host = socket.gethostname()
-        print(f"Client : {host}")
-        message = conn.recv(1024).decode()
-        print(f"Message reçu : {message}")
+        reply = str(input("Serveur :"))
+        conn.send(reply.encode())
 
-        if message == "stop":
-            reply = "Au revoir"
-            print("Fermeture de la connexion")
-            conn.send(reply.encode())
-            conn.close()
-            server_socket.close()
-            flag = True
-        elif message == "bye":
-            print("En attente d'un nouveau client")
-            server_socket.listen(1)
-            conn, address = server_socket.accept()
-        else:
-            reply = str(input("Réponse du serveur :"))
-            conn.send(reply.encode())
-
-            if reply == "bye":
-                print("En attente d'un nouveau client")
-                server_socket.listen(1)
-                conn, address = server_socket.accept()
