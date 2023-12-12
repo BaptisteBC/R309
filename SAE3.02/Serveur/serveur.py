e9363f896aa5861921749959b3de26e0d69fb268
baptisteBC <baptiste.blanccarrier@gmail.com>
2023-12-12 15:24:10 +0100

    12/12

--- SAE3.02/Serveur/serveur.py
+++ SAE3.02/Serveur/serveur.py
@@ -2,48 +2,46 @@ import socket
 import threading
 
 
-def ecoute(conn, stop_sending):
-    msg = ""
-    while msg != "bye" and msg != "stop" and not stop_sending.is_set():
-        msg = conn.recv(1024).decode()
-        print(f"Client : \n {msg}")
-    reply = "bye"
-    conn.send(reply.encode())
-    stop_sending.set()
+class Serveur():
+    def __init__(self):
+        self.reply = ""
+        self.serveursocket = socket.socket()
+        self.nb_clients = 2
 
+    def main(self):
+        flag = False
+        port = 10000
+        stop_sending = threading.Event()
 
-def accept():
+        self.serveursocket.bind(('0.0.0.0', port))
+        print(f"Serveur allumé")
 
+        self.serveursocket.listen(self.nb_clients)
+
+        while not flag:
+
+            conn, address = self.serveursocket.accept()
+            print(conn, address)
+
+            messClients = threading.Thread(target=self.ecoute, args=[conn, stop_sending])
+            messClients.start()
+
+            while self.reply != "stop" and not stop_sending.is_set():
+                reply = str(input("Serveur : "))
+                conn.send(reply.encode())
+            stop_sending.set()
+
+            messClients.join()
+
+    def ecoute(self, conn, stop_sending):
+        msg = ""
+        while msg != "bye" and msg != "stop" and not stop_sending.is_set():
+            msg = conn.recv(1024).decode()
+            print(f"Client : \n {msg}")
+        reply = "bye"
+        conn.send(reply.encode())
+        stop_sending.set()
 
 
 if __name__ == '__main__':
-    flag = False
-    port = 10000
-    reply = ""
-    stop_sending = threading.Event()
-    serveursocket = socket.socket()
-    nb_clients = 2
-
-    serveursocket.bind(('0.0.0.0', port))
-    print(f"Serveur allumé")
-
-    serveursocket.listen(nb_clients)
-
-    while not flag:
-        for i in range(nb_clients):
-            conn, address = serveursocket.accept()
-            print(conn, address)
-
-        accept = threading.Thread(target=accept)
-        accept.start()
-
-        messClients = threading.Thread(target=ecoute, args=[conn, stop_sending])
-        messClients.start()
-
-        while reply != "stop" and not stop_sending.is_set():
-            reply = str(input("Serveur : "))
-            conn.send(reply.encode())
-        stop_sending.set()
-
-        messClients.join()
-        accept.join()
+    Serveur.main(Serveur)
---
+++ SAE3.02/Serveur/serveur2.0.py
@@ -0,0 +1,10 @@
+import socket
+import threading
+
+class Serveur():
+    def __init__(self):
+        self.
+    def main(self):
+
+if __name__ == "__main__":
+    pass
