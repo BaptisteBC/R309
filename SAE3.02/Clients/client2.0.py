324c51e027177e6db9c7a6e47ac8c91ca5f7136b
BaptisteBC <baptiste.blanccarrier@gmail.com>
2023-12-18 18:26:35 +0100

    Lancement fenêtre tchat à partir identification

--- SAE3.02/Clients/client2.0.py
+++ SAE3.02/Clients/client2.0.py
@@ -87,6 +87,10 @@ class Identification(QMainWindow):
                 print(reply)
                 if reply == "auth_OK":
                     self.reply.append(f"Bienvenue {identifiant} !")
+                    self.window = Client(self.clientsocket)
+                    self.window.show()
+
+
                 elif reply == "auth_stop":
                     print("Trop de tentatives infructueuses, fin de la connexion.")
                     self.clientsocket.close()
@@ -94,37 +98,20 @@ class Identification(QMainWindow):
                 else:
                     self.reply.append(reply)
 
-    def ecoute(self):
-        reply = ""
-        while reply != "bye" and reply != "stop" and not self.stop_sending.is_set():
-            reply = self.clientsocket.recv(1024).decode()
-            self.tchat.append(f"CLient : {reply}")
 
-        msg = "bye"
-        self.clientsocket.send(msg.encode())
-        self.stop_sending.set()
-
-
-class Client(QMainWindow):
-    def __init__(self):
+class Client(QWidget):
+    def __init__(self, clientsocket):
         super().__init__()
 
         self.listen = None
-        self.clientsocket = socket.socket()
+        self.clientsocket = clientsocket
 
         self.stop_sending = None
         self.setWindowTitle("Client de tchat")
-        widget = QWidget()
-        self.setCentralWidget(widget)
         grid = QGridLayout()
-        widget.setLayout(grid)
+        self.setLayout(grid)
         self.resize(300, 300)
 
-        self.labServ = QLabel("@IP Serveur")
-        self.ipServ = QLineEdit('127.0.0.1')
-        self.labPort = QLabel("Port")
-        self.portServ = QLineEdit("10000")
-        self.connectButton = QPushButton("Connexion")
         self.labMessage = QLabel("Votre message :")
         self.message = QLineEdit()
         self.envoi = QPushButton("Envoi")
@@ -132,58 +119,15 @@ class Client(QMainWindow):
         self.tchat.setReadOnly(True)
         self.quitButton = QPushButton("Quitter")
 
-        grid.addWidget(self.labServ, 0, 0, 1, 1)
-        grid.addWidget(self.ipServ, 0, 1, 1, 1)
-        grid.addWidget(self.labPort, 1, 0, 1, 1)
-        grid.addWidget(self.portServ, 1, 1, 1, 1)
-        grid.addWidget(self.connectButton, 2, 0, 1, 2)
-        grid.addWidget(self.labMessage, 3, 0, 1, 1)
-        grid.addWidget(self.envoi, 3, 1, 1, 1)
-        grid.addWidget(self.message, 4, 0, 1, 2)
-        grid.addWidget(self.tchat, 5, 0, 1, 2)
-        grid.addWidget(self.quitButton, 6, 0, 1, 2)
-
-        self.quitButton.clicked.connect(self.quitter)
-        self.connectButton.clicked.connect(self.connexion)
-        self.envoi.clicked.connect(self.envoi_msg)
-
-    def quitter(self):
-        self.clientsocket.send("bye".encode())
-        self.stop_sending.set()
-        self.listen.join()
-        self.clientsocket.close()
-        QCoreApplication.exit(0)
-
-    def ecoute(self):
-        reply = ""
-        while reply != "bye" and reply != "stop" and not self.stop_sending.is_set():
-            reply = self.clientsocket.recv(1024).decode()
-            self.tchat.append(f"CLient : {reply}")
-
-        msg = "bye"
-        self.clientsocket.send(msg.encode())
-        self.stop_sending.set()
-
-    def connexion(self):
-        self.stop_sending = threading.Event()
-        self.tchat.append("Tentative de connexion")
-        self.clientsocket.connect((self.ipServ.text(), int(self.portServ.text())))
-        self.tchat.append("Client connecté")
-
-        self.listen = threading.Thread(target=self.ecoute)
-        self.listen.start()
-
-    def envoi_msg(self):
-        msg = self.message.text()
-        self.tchat.append(f"Moi : {msg}")
-        print(f"J'envoie {msg}")
-        self.clientsocket.send(msg.encode())
+        grid.addWidget(self.labMessage)
+        grid.addWidget(self.envoi)
+        grid.addWidget(self.message)
+        grid.addWidget(self.tchat)
+        grid.addWidget(self.quitButton)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     identification = Identification()
     identification.show()
-    # window = Client()
-    # window.show()
     app.exec()
--- SAE3.02/Serveur/serveur2.0.py
+++ SAE3.02/Serveur/serveur2.0.py
@@ -4,57 +4,25 @@ import mysql.connector
 
 
 class Serveur:
-    def __init__(self, ip='0.0.0.0', port=10000):
+    def __init__(self, ip='0.0.0.0', port=10000, max_client=2, sql_user='root', sql_mdp='toto', sql_host='127.0.0.1'):
         self.server_socket = socket.socket()
         self.ip = ip
         self.port = port
-        self.max_client = 2
+        self.max_client = max_client
         self.msg = ""
         self.reply = ""
         self.stop_sending = threading.Event()
-        self.cnx = mysql.connector.connect(user='root', password='toto', host='127.0.0.1', database='test')
+        self.cnx = mysql.connector.connect(user=sql_user, password=sql_mdp, host=sql_host, database='test')
         self.liste_client = []
-
-    def admin(self):
-        flag = False
-        essais = 3
-        while not flag:
-            # identifiant = str(input("Identifiant : "))
-            # mdp = str(input("Mot de passe : "))
-            identifiant = "toto"
-            mdp = "toto"
-
-            cursor = self.cnx.cursor()
-            cursor.execute(f"SELECT * FROM login where nom like '{identifiant}';")
-            results = cursor.fetchall()
-            if not results:
-                print("Identifiant invalide")
-                essais -= 1
-                if essais == 0:
-                    print("Trop de tentatives infructueuses")
-                    flag = True
-            else:
-                result = results[0]
-                # print(result)
-                cursor.close()
-
-                if result[2] == mdp:
-                    print(f"Connexion réussie \n Bienvenue Administrateur !")
-                    self.main()
-
-                    flag = True
-                else:
-                    print("Mauvais mot de passe")
-                    essais -= 1
-                    if essais == 0:
-                        print("Trop de tentatives infructueuses")
-                        flag = True
+        self.stop_serveur = False
 
     def accept(self, conn):
         reply = "Client connecté !"
-        print(reply)
+        print(reply, conn)
         conn.send(reply.encode())
+
         self.auth(conn)
+
         listen = threading.Thread(target=self.ecoute, args=[conn])
         listen.start()
 
@@ -100,7 +68,9 @@ class Serveur:
 
                     flag = True
                 else:
-                    print("Mauvais mot de passe")
+                    reply = "Mauvais mot de passe."
+                    print(reply)
+                    conn.send(reply.encode())
                     essais -= 1
                     if essais == 0:
                         print("Trop de tentatives infructueuses")
@@ -112,18 +82,17 @@ class Serveur:
         self.server_socket.bind((self.ip, self.port))
         self.server_socket.listen(self.max_client)
 
-        for i in range(self.max_client):
+        while not self.stop_serveur:
             conn, address = self.server_socket.accept()
             self.liste_client.append(conn)
             client_accept = threading.Thread(target=self.accept, args=[conn])
             client_accept.start()
 
     def ecoute(self, conn):
-        print(self.reply)
         msg = ""
-        while msg != "bye" and msg != "stop" and not self.stop_sending.is_set():
+        while msg != "bye" and msg != "stop" and not self.stop_sending.is_set() and not self.stop_serveur:
             msg = conn.recv(1024).decode()
-            print(f"Client : \n {msg}")
+            print(f"Client : {msg}")
             self.write_bdd(msg)
             for i in range(len(self.liste_client)):
                 if self.liste_client[i] == conn or msg == "bye":
@@ -143,10 +112,7 @@ class Serveur:
         self.cnx.commit()
         cursor.close()
 
-    def id_client(self):
-        pass
-
 
 if __name__ == "__main__":
     serveur = Serveur()
-    serveur.admin()
+    serveur.main()
