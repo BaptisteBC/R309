01aa4b1e1ae909ac08b3e9cc73717571e1e57d24
BaptisteBC <baptiste.blanccarrier@gmail.com>
2023-12-28 18:41:17 +0100

    Add files via upload

---
+++ SAE3.02/sae302-dump.sql
@@ -0,0 +1,160 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: SAE302
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ip`
+--
+
+DROP TABLE IF EXISTS `ip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ip` (
+  `idClient` int NOT NULL,
+  `Adresse_IP` varchar(50) NOT NULL,
+  KEY `idClient` (`idClient`),
+  CONSTRAINT `ip_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `login` (`idClient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ip`
+--
+
+LOCK TABLES `ip` WRITE;
+/*!40000 ALTER TABLE `ip` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ip` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `journal`
+--
+
+DROP TABLE IF EXISTS `journal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `journal` (
+  `idSalon` int NOT NULL,
+  `Nom_Salon` varchar(100) DEFAULT NULL,
+  `idClient` int NOT NULL,
+  `Alias` varchar(100) DEFAULT NULL,
+  `Message` varchar(500) DEFAULT NULL,
+  `Date` datetime DEFAULT NULL,
+  KEY `idSalon` (`idSalon`),
+  KEY `idClient` (`idClient`),
+  CONSTRAINT `journal_ibfk_1` FOREIGN KEY (`idSalon`) REFERENCES `salons` (`idSalon`),
+  CONSTRAINT `journal_ibfk_2` FOREIGN KEY (`idClient`) REFERENCES `login` (`idClient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `journal`
+--
+
+LOCK TABLES `journal` WRITE;
+/*!40000 ALTER TABLE `journal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `journal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `login` (
+  `idClient` int NOT NULL AUTO_INCREMENT,
+  `Alias` varchar(100) NOT NULL,
+  `Mot_de_Passe` varchar(100) NOT NULL,
+  `banned` tinyint NOT NULL DEFAULT '0',
+  `kick` datetime DEFAULT NULL,
+  PRIMARY KEY (`idClient`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (1,'Admin','Admin',0,NULL);
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permissions` (
+  `idClient` int NOT NULL,
+  `idSalon` int NOT NULL,
+  `Permission` tinyint NOT NULL DEFAULT '0',
+  KEY `idClient` (`idClient`),
+  KEY `idSalon` (`idSalon`),
+  CONSTRAINT `permissions_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `login` (`idClient`),
+  CONSTRAINT `permissions_ibfk_2` FOREIGN KEY (`idSalon`) REFERENCES `salons` (`idSalon`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (1,1,1),(1,2,1),(1,3,1),(1,4,1),(1,5,1);
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salons`
+--
+
+DROP TABLE IF EXISTS `salons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salons` (
+  `idSalon` int NOT NULL AUTO_INCREMENT,
+  `Nom_Salon` varchar(100) NOT NULL,
+  PRIMARY KEY (`idSalon`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salons`
+--
+
+LOCK TABLES `salons` WRITE;
+/*!40000 ALTER TABLE `salons` DISABLE KEYS */;
+INSERT INTO `salons` VALUES (1,'Général'),(2,'Blabla'),(3,'Marketing'),(4,'Informatique'),(5,'Comptabilité');
+/*!40000 ALTER TABLE `salons` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-28 12:31:28
